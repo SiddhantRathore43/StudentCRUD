190363758bbca05cb5e3e2a823963c48c9c8a258
SiddhantRathore43 <siddhantrathore36@gmail.com>
2026-06-23 19:17:15 +0530

    Added/Updated

--- src/main/java/com/crud/work/repository/EmpRepo.java
+++ src/main/java/com/crud/work/repository/EmpRepo.java
@@ -3,10 +3,12 @@ package com.crud.work.repository;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.crud.work.entity.Employee;
 import com.crud.work.enums.Department;
 
+@Repository
 public interface EmpRepo extends JpaRepository<Employee,Long> {
 	
 	 List<Employee> findByDepartment(Department department);
